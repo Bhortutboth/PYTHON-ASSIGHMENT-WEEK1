933f41dd48a3fbfb7381c3cb6be5033f437f6289
Bhortutboth <bhortut115@gmail.com>
2026-09-02 11:21:56 +0300

    Add files via upload

--- python.py
+++ python.py
@@ -3,7 +3,7 @@ age = "29"
 favorite_language="Python"
 goal="to become a software engineer"
 # Displaying output using the variables
-print("Hello, my name is", name + ".")
-print("I am", age, "years old.")
+print("Hello, my name is", name + "Bor.")
+print("I am", age + "29", "years old.")
 print("My favourite programming language is", favourite_language + "PYTHON.")
 print("My goal for this course is", goal + "to become a software engineer.")
