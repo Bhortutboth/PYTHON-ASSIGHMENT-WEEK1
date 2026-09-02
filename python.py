30199737a67a6de82f37c05a7942bc42302c4335
Bhortutboth <bhortut115@gmail.com>
2026-09-02 11:29:59 +0300

    Add files via upload
    my first commit

--- python.py
+++ python.py
@@ -5,5 +5,5 @@ goal="to become a software engineer"
 # Displaying output using the variables
 print("Hello, my name is", name + "Bor.")
 print("I am", age + "29", "years old.")
-print("My favourite programming language is", favourite_language + "PYTHON.")
+print("My favourite programming language is", favourite_language + "Python.")
 print("My goal for this course is", goal + "to become a software engineer.")
